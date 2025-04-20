0d5d82d6ca9e4d969656de20eea11a6a40e48ac8
tananusin <tananusin@gmail.com>
2025-04-20 13:44:38 +0700

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,25 +1,26 @@
 # fetch.py
 import yfinance as yf
 from datetime import datetime
+from zoneinfo import ZoneInfo  # Requires Python 3.9+
 
 def get_price(symbol):
     symbol_clean = str(symbol).strip().upper()
     if symbol_clean.startswith(("CASH", "BOND")):
-        return 1.0, datetime.now()
+        return 1.0, datetime.now(THAILAND_TZ)
     try:
         ticker = yf.Ticker(symbol_clean)
         price = ticker.info["regularMarketPrice"]        
-        return price, datetime.now()
+        return price, datetime.now(THAILAND_TZ)
     except:
         return None, None
 
 def get_fx_to_thb(currency):
     if currency == "THB":
-        return 1.0
+        return 1.0, datetime.now(THAILAND_TZ)
     try:
         pair = f"{currency}THB=X"
         fx = yf.Ticker(pair).history(period="1d")
         fx_rate = round(fx["Close"].iloc[-1], 2)
-        return fx_rate, datetime.now()
+        return fx_rate, datetime.now(THAILAND_TZ)
     except:
         return None, None
