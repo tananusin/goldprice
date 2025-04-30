885ad1e3ed71f5d1e59b6264ae0f7e6c2a71a77f
T-Nai <tananusin@gmail.com>
2025-04-30 11:47:44 +0700

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,25 +1,22 @@
 # fetch.py
-import yfinance as yf
+from yahooquery import Ticker
 from datetime import datetime
 
 def get_price(symbol):
-    symbol_clean = str(symbol).strip().upper()
-    if symbol_clean.startswith(("CASH", "BOND")):
-        return 1.0, datetime.now()
     try:
-        ticker = yf.Ticker(symbol_clean)
-        price = ticker.info["regularMarketPrice"]
+        ticker = Ticker(symbol)
+        price = ticker.price['regularMarketPrice']
         return price, datetime.now()
-    except:
+    except Exception as e:
+        print(f"Error fetching data for {symbol}: {e}")
         return None, None
 
 def get_fx_to_thb(currency):
-    if currency == "THB":
-        return 1.0, datetime.now()
     try:
         pair = f"{currency}THB=X"
-        fx = yf.Ticker(pair).history(period="1d")
-        fx_rate = round(fx["Close"].iloc[-1], 2)
+        ticker = Ticker(pair)
+        fx_rate = ticker.price['regularMarketPrice']
         return fx_rate, datetime.now()
-    except:
+    except Exception as e:
+        print(f"Error fetching FX data for {currency} to THB: {e}")
         return None, None
